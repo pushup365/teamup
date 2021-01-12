693515908f477f9c96f32d339bb144d51f438fdc
Kenneth Cheung <pushup365@hotmail.com>
2021-01-12 23:37:50 +0100

    AuthenticationAdded

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,5 +5,8 @@ namespace API.Entities
         public int Id { get; set; }
         // asp.net identity
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
     }
 }
